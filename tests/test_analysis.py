a8f979f8c59b69224000a8c52ee7df5df4f1d3da
Tristan Glatard <tristan.glatard@concordia.ca>
2019-10-15 11:28:21 -0400

    Fixed syntax error

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -4,8 +4,8 @@ def test_average():
     avg = analysis.average_inflammations(0)
     assert(avg == 5.45)
 
-def test_max()
+def test_max():
     raise("Remove this line and implement your test instead")
 
-def test_acute_patient()
+def test_acute_patient():
     raise("Remove this line and implement your test instead")
